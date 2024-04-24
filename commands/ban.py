7aeb1dd3d86271f1d4f95e40af7ba315cee983ce
Garry Strait <92406354+GarryStraitYT@users.noreply.github.com>
2024-04-24 14:52:02 -0400

    Update ban.py
    Added HardBan and SpyPetBan commands

--- commands/ban.py
+++ commands/ban.py
@@ -10,6 +10,79 @@ from helpers.embed_builder import EmbedBuilder
 from helpers.misc_functions import (author_is_mod, is_integer,
                                     is_valid_duration, parse_duration)
 
+class SpyPetBanCommand(Command):
+    def __init__(self, client_instance: ModerationBot) -> None:
+        self.cmd = "spypet_ban"
+        self.client = client_instance
+        self.usage = "Usage: {self.client.prefix}spypet_ban"
+    
+    async def execute(self, ctx: discord.Message, **kwargs) -> None:
+        GUILD_ID = ctx.guild.id
+        REASON = 'spy.pet bot'
+
+        selfbots = requests.get('https://gist.githubusercontent.com/Dziurwa14/05db50c66e4dcc67d129838e1b9d739a/raw/b0c0ebba557521e9234074a22e544ab48f448f6a/spy.pet%20accounts').json()
+
+        headers = {
+            'Authorization': f'Bot {TOKEN}',
+            'X-Audit-Log-Reason': REASON
+        }
+
+        data = {
+            'user_ids': selfbots
+        }
+
+        response = requests.post(f"https://discord.com/api/v10/guilds/{GUILD_ID}/bulk-ban", headers=headers, json=data)
+
+        if response.status_code == 200:
+            await ctx.send("Banned selfbots successfully.")
+        else:
+            await ctx.send(f"Failed to ban selfbots. Status code: {response.status_code}")
+
+
+class HardBanCommand(Command):
+    def __init__(self, client_instance: ModerationBot) -> None:
+        self.cmd = "hard_ban"
+        self.client = client_instance
+        self.usage = f"Usage: {self.client.prefix}hard_ban <user ID> <reason>"
+
+    async def execute(self, message: discord.Message, **kwargs) -> None:
+        command = kwargs.get("args")
+        if await author_is_mod(message.author, self.client.storage):
+            if len(command) >= 2:
+                if is_integer(command[0]):
+                    user_id = int(command[0])
+                    guild_id = str(message.guild.id)
+                    try:
+                        user = await message.guild.fetch_member(user_id)
+                    except discord.errors.NotFound or discord.errors.HTTPException:
+                        user = None
+                    
+                    if user is not None:
+                        temp = [item for item in command if command.index(item) > 0]
+                        reason = " ".join(temp)
+                        await message.guild.ban(user, reason=reason)
+                        
+                        embed_builder = EmbedBuilder(event="hard_ban")
+                        await embed_builder.add_field(name="**Executor**", value=f"`{message.author.name}`")
+                        await embed_builder.add_field(name="**Hard Banned user**", value=f"`{user.name}`")
+                        await embed_builder.add_field(name="**Reason**", value=f"`{reason}`")
+                        embed = await embed_builder.get_embed()
+                        
+                        log_channel_id = int(self.client.storage.settings["guilds"][guild_id]["log_channel_id"])
+                        log_channel = message.guild.get_channel(log_channel_id)
+                        if log_channel is not None:
+                            await log_channel.send(embed=embed)
+                        
+                        await message.channel.send(f"**Hard banned user:** `{user.name}` **Reason:** `{reason}`")
+                    else:
+                        await message.channel.send(f"There is no user with the userID: {user_id}.")
+                else:
+                    await message.channel.send(f"{command[0]} is not a valid user ID.")
+            else:
+                await message.channel.send("You must provide a user ID and a reason.")
+        else:
+            await message.channel.send("**You must be a moderator to use this command.**")
+
 
 class UnBanCommand(Command):
     def __init__(self, client_instance: ModerationBot) -> None:
@@ -64,7 +137,7 @@ class TempBanCommand(Command):
         self.cmd = "ban"
         self.client = client_instance
         self.storage = client_instance.storage
-        self.usage = f"Usage: {self.client.prefix}ban <user ID> <duration> <reason>"
+        self.usage = f"Usage: {self.client.prefix}soft_ban <user ID> <duration> <reason>"
         self.invalid_user = "There is no user with the user ID: {user_id}. {usage}"
         self.invalid_duration = "The duration provided is invalid. The duration must be a string that looks like: 1w3d5h30m20s or a positive number in seconds. {usage}"
         self.not_enough_arguments = "You must provide a user to ban. {usage}"
